42571724a552c89fe787edcdc625e8d6c9bfd692
wintercricket <hodhodhod2003@gmail.com>
2021-03-29 00:56:35 -0500

    add select statement

--- DBDirectory/includes/functions.php
+++ DBDirectory/includes/functions.php
@@ -12,3 +12,17 @@ function formatcode($arr){
 
 /* select statement */
 
+function selectAll(){
+	global $mysqli;
+	$stmt = $mysqli->prepare('SELECT * FROM employees');
+	$stmt->execute();
+	$result = $stmt->get_result();
+
+	if($result->num_rows === 0){
+		echo 'No rows';//echo ('No rows');
+	}
+	while($row = $result->fetch_assoc()){
+		$data[] = $row;
+	}
+	$stmt->close();
+}
