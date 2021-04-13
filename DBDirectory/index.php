72f26af33ec5d8d6233d39e1628f4a4878f510c1
wintercricket <hodhodhod2003@gmail.com>
2021-04-12 20:02:52 -0500

    This is a push from the index instead of readme

--- DBDirectory/index.php
+++ DBDirectory/index.php
@@ -35,6 +35,7 @@ include('includes/functions.php');
 
 
 		 <h3>This is the new index addition on the secretPHP branch, now merged with the master</h3>
+		 <h3>This is a push version</h3>
 
 	</div>
 </body>
